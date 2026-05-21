1864f6840f58670e1873ef3c6e1018aa2f2a25e4
Estevam <estvmc@gmail.com>
2026-05-20 21:06:10 -0300

    feat: solucao exercicio cond01

---
+++ cond01.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class cond01 {
+
+	public static void main (String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		if(num < 0) {
+			System.out.print("NEGATIVO");
+		} else {
+			System.out.print("POSITIVO");
+		}
+		
+		sc.close();
+	}
+}
